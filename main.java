f4c2151bb94265a71434c01243568552a035afee
Bonor Ayambem <bma223@lehigh.edu>
2023-04-02 17:40:04 -0400

    Scoring Completed

--- main.java
+++ main.java
@@ -23,8 +23,8 @@ public class main {
         }
         System.out.println("Input sequences are as follows: \n" + sequence1 + "\n" + sequence2);
 
-        int m = sequence1.length();
-        int n = sequence2.length();
+        int m = sequence2.length();
+        int n = sequence1.length();
 
         double[][] mGrid = new double[m+1][n+1];
         double[][] xGrid = new double[m+1][n+1];
@@ -34,32 +34,32 @@ public class main {
         initializeGrids(mGrid, xGrid, yGrid);
 
         // fill in the rest of the grids
-        for(int i = 1; i < m; i++){
-            for(int j = 1; j < n; j++){
-                double x1 = -10 + -0.5 + mGrid[i][j-1];
+        for(int i = 1; i < m+1; i++){
+            for(int j = 1; j < n+1; j++){
+                double x1 = -10 - 0.5 + mGrid[i][j-1];
                 double x2 = -0.5 + xGrid[i][j-1];
-                double x3 = -10 + -0.5 + yGrid[i][j-1];
+                double x3 = -10 - 0.5 + yGrid[i][j-1];
 
-                x[i][j] = Math.max(x1, x2);
-                x[i][j] = Math.max(x[i][j], x3);
+                xGrid[i][j] = Math.max(x1, x2);
+                xGrid[i][j] = Math.max(xGrid[i][j], x3);
 
 
                 double y1 = -10 + -0.5 + mGrid[i-1][j];
                 double y2 = -0.5 + yGrid[i-1][j];
                 double y3 = -10 + -0.5 + xGrid[i-1][j];
 
-                y[i][j] = Math.max(y1, y2);
-                y[i][j] = Math.max(y[i][j], y3);
+                yGrid[i][j] = Math.max(y1, y2);
+                yGrid[i][j] = Math.max(yGrid[i][j], y3);
 
 
-                int match = sequence1.charAt(i-1) == sequence2.charAt(i-1) ? 1 : -4;
+                int match = sequence2.charAt(i-1) == sequence1.charAt(j-1) ? 1 : -4;
 
                 double m1 = match + mGrid[i-1][j-1];
                 double m2 = match + xGrid[i][j];
                 double m3 = match + yGrid[i][j];
 
-                x[i][j] = Math.max(x1, x2);
-                x[i][j] = Math.max(x[i][j], x3);
+                mGrid[i][j] = Math.max(m1, m2);
+                mGrid[i][j] = Math.max(mGrid[i][j], m3);
             }
         }
 
@@ -80,23 +80,26 @@ public class main {
 
         // first rows
         for(int i = 1; i < m[0].length; i++){
-            m[0][i] = Double.MIN_VALUE;
-            y[0][i] = Double.MIN_VALUE;
-            x[0][i] = -10.5 - (0.5 * i);
+            m[0][i] = -Double.MAX_VALUE;
+            y[0][i] = -Double.MAX_VALUE;
+            x[0][i] = -10.5 - (0.5 * (i-1));
         }
 
         // first columns
         for(int i = 1; i < m.length; i++){
-            m[i][0] = Double.MIN_VALUE;
-            x[i][0] = Double.MIN_VALUE;
-            y[i][0] = -10.5 - (0.5 * i);
+            m[i][0] = -Double.MAX_VALUE;
+            x[i][0] = -Double.MAX_VALUE;
+            y[i][0] = -10.5 - (0.5 * (i-1));
         }
     }
 
     public static void printGrid(double[][] grid){
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[i].length; j++){
-                System.out.print(grid[i][j] + "   \t");
+                if(grid[i][j] == -Double.MAX_VALUE){
+                    System.out.print("-INF" + "\t\t");
+                }
+                else System.out.print(grid[i][j] + "\t\t");
             }
             System.out.println();
         }
